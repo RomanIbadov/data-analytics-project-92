54d7b840dc272a09511fef79875afc29919c353f
RomanIbadov <romanibadov0@gmail.com>
2024-07-20 15:09:47 +0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,16 +1,17 @@
-ШАГ 4
-считает общее количество покупателей из таблицы customers
+--ШАГ 4--
+---считает общее количество покупателей из таблицы customers
 select count(customer_id) as "customers_count"
 from public.customers;
 
-ШАГ 5
-Первый отчет о десятке лучших продавцов.
+--ШАГ 5--
+---Первый отчет о десятке лучших продавцов.
 select
     concat(public.employees.first_name, ' ', public.employees.last_name)
     as seller,
     count(public.sales.sales_person_id)
     as operations,
-    floor(sum(public.products.price * public.sales.quantity)) as income
+    floor(sum(public.products.price * public.sales.quantity))
+    as income
 from public.employees
 inner join public.sales
     on public.employees.employee_id = public.sales.sales_person_id
@@ -20,7 +21,7 @@ group by seller
 order by income desc
 limit 10;
 
-"Второй отчет о продавцах,средняя выручка меньше средней выручкипо всем продавцам"
+---отчет о продавцах,средняя выручка меньше средней выручкипо всем
 select
     concat(public.employees.first_name, ' ', public.employees.last_name)
     as seller,
@@ -40,7 +41,8 @@ having
             on public.sales.product_id = public.products.product_id
     )
 order by average_income;
-"Третий отчет содержит информацию о выручке по дням недели"
+
+---Третий отчет содержит информацию о выручке по дням недели
 with dow as (
     select
         concat(public.employees.first_name, ' ', public.employees.last_name)
@@ -63,8 +65,8 @@ select
 from dow
 order by day_week, seller;
 
-ШАГ 6
-"отчет количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+"
+--ШАГ 6--
+---отчет количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+
 select
     (
         case
@@ -79,7 +81,7 @@ from public.customers
 group by age_category
 order by age_category;
 
-"отчет по количеству уникальных покупателей и выручке, которую они принесли"
+---отчет по количеству уникальных покупателей и выручке, которую они принесли
 select
     to_char(public.sales.sale_date, 'YYYY-MM') as selling_month,
     count(distinct public.sales.customer_id) as total_customers,
@@ -90,7 +92,7 @@ inner join public.products
 group by selling_month
 order by selling_month;
 
-"отчет о покупателях, первая покупка которых была в ходе проведения акций"
+---отчет о покупателях, первая покупка которых была в ходе проведения акций
 with tab1 as (
     select
         public.sales.sale_date,
